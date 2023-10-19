c3521ae3d13a09f8e6b847e8e0fc0a11da2f9389
Adam Papula <adam@papula.cz>
2023-10-20 00:20:14 +0200

    workflow test hub

--- compile.sh
+++ compile.sh
@@ -15,16 +15,18 @@ then
     if [ -d "$DIRECTORY" ] && [ "$DIRECTORY" != "Assets/" -a "$DIRECTORY" != "Styles/" -a "$DIRECTORY" != "releases/" ]
     then   
         cd "$DIRECTORY" || continue
-        TEX_FILE=$(find . -name "*.tex" -maxdepth 1)
-        echo Compiling LaTeX file "$TEX_FILE" in "$DIRECTORY"
-        if [ -n "$TEX_FILE" ]
-        then
-            lualatex  --synctex=0 --interaction=nonstopmode --output-directory="../releases/" --jobname="${DIRECTORY%/}" "$TEX_FILE"
-            lualatex  --synctex=0 --interaction=nonstopmode --output-directory="../releases/" --jobname="${DIRECTORY%/}" "$TEX_FILE"
-            lualatex  --synctex=0 --interaction=nonstopmode --output-directory="../releases/" --jobname="${DIRECTORY%/}" "$TEX_FILE"
-        else
-            echo "No LaTeX file found, exit."
-        fi
+        # TEX_FILE=$(find . -name "*.tex" -maxdepth 1)
+        # echo Compiling LaTeX file "$TEX_FILE" in "$DIRECTORY"
+        # if [ -n "$TEX_FILE" ]
+        # then
+            # lualatex  --synctex=0 --interaction=nonstopmode --output-directory="../releases/" --jobname="${DIRECTORY%/}" "$TEX_FILE"
+            # lualatex  --synctex=0 --interaction=nonstopmode --output-directory="../releases/" --jobname="${DIRECTORY%/}" "$TEX_FILE"
+            # lualatex  --synctex=0 --interaction=nonstopmode --output-directory="../releases/" --jobname="${DIRECTORY%/}" "$TEX_FILE"
+        # else
+            # echo "No LaTeX file found, exit."
+        # fi
+        PDF_FILE=$(find . -name "*.pdf" -maxdepth 1)
+        mv $PDF_FILE ../releases/${DIRECTORY%/}.pdf
         cd "$CURRENT_PATH"
     fi
     done
